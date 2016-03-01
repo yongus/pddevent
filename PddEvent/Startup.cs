e0fb244670b4dd6262b0f9c139dd82c2dc122572
evindj <djiofack007@gmail.com>
2016-02-29 21:23:29 -0500

    working hard

---
+++ PddEvent/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PddEvent.Startup))]
+namespace PddEvent
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
